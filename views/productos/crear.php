9504f8ad1143e3a117f88900bcc07281ad16a9a4
kelvindominguez1595 <kelvin.dominguez95@gmail.com>
2020-11-01 21:34:01 -0600

    Terminando estilo de vista crear productos

--- views/productos/crear.php
+++ views/productos/crear.php
@@ -14,23 +14,25 @@
                     <form action="?view=Productos&action=CrearProducto" method="post" class="user" enctype="multipart/form-data">
                     <div class="form-group row">
                         <div class="col-sm-6 mb-3 mb-sm-0">
+                            <label for="cantidad">Nombre del producto</label>
                             <input type="text" name="NombreProducto" class="form-control form-control-user" required id="exampleFirstName" placeholder="Nombre del Producto">
                         </div>
                         <div class="col-sm-2">
+                            <label for="cantidad">Cantidad</label>
                             <input type="number" name="cantidad" id="" min="0"  class="form-control form-control-user" placeholder="Cantidad">
                         </div>
                         <div class="col-sm-2">
+                            <label for="pcompra">P. Compra</label>
                             <input type="number" step="any" min="0" name="precioCompra" class="form-control form-control-user" required id="exampleFirstName" placeholder="$ Compra">
                         </div>
                         <div class="col-sm-2">
+                            <label for="precioVenta">P. Venta</label>
                             <input type="number" step="any" min="0"  name="precioVenta" class="form-control form-control-user" required id="exampleFirstName" placeholder="$ Venta">
                         </div>
                     </div>
                     <div class="form-group row">
                         <div class="col-sm-4">
-                            <input type="file" name="imagen" id="" class="form-control" required placeholder="Imagen">
-                        </div>
-                        <div class="col-sm-4">
+                            <label for="categoria">Categoria</label>
                             <select name="id_categoria" id="" class="form-control" required>
                                     <option value="0">Seleccione categoria</option>
                                     <option value="1">Mouse</option>
@@ -38,11 +40,19 @@
                             </select>
                         </div>
                         <div class="col-sm-4">
+                            <label for="categoria">Marca</label>
                             <select name="id_marca_producto" id="" class="form-control" required>
                                 <option value="0">Seleccione marca</option>
                                 <option value="1">HP</option>
                                 <option value="2">Leonovo</option>
                             </select>
+                        </div>
+                        <div class="col-sm-4">
+                            <label>Subir imagen</label>
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="customFileLang" name="imagen"  lang="es">
+                                <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label>
+                            </div>
                         </div>
                     </div>
                     <div class="form-group row d-flex justify-content-center">
