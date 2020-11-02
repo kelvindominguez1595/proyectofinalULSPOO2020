d29b0e257ce9a5478b6054a9a27d55112c61eb92
SergioVentura1 <73601111+SergioVentura1@users.noreply.github.com>
2020-11-01 18:24:00 -0600

    Add files via upload

---
+++ Model/Marcas.php
@@ -0,0 +1,98 @@
+<?php
+class Marcas{
+    # atributos 
+    private $DB; // para la conexion de la base de datos
+    public $id_marca;
+    public $nombre_marca;
+
+
+    public function __CONSTRUCT(){
+        try{
+            $this->DB = Database::Conexion();
+        }catch(Throwable $t){
+            die($t->getMessage());
+        }
+    }
+
+    public function RegistrarMarca($data){
+        try{
+            // Comando SQL
+            $sql = "INSERT INTO productos(id, nombre_marca) VALUES(?,?)";
+            // COMENZAMOS LA CONEXION CON PDO
+            $pre = $this->DB->prepare($sql);
+            $resul = $pre->execute(array($data->id, $data->nombre_marca));
+            if($resul > 0){ 
+                return true;
+            }else{ 
+                return false;
+            }
+        }catch(Throwable $t){
+            die($t->getMessage());
+        }
+    }
+    // Metodo para listar los roles
+    public function ListarMarcas(){
+        try{        
+            $commd = $this->DB->prepare("SELECT * FROM marcas");
+            $commd->execute();
+            return $commd->fetchAll(PDO::FETCH_OBJ);
+        }catch(Throwable $t){
+            die($t->getMessage());
+        }
+    }
+
+    // Metodo para obtener un registro en especifico
+    public function obtenerRegistro($id){
+        try{        
+            $commd = $this->DB->prepare("SELECT * FROM marcas WHERE id = ?");
+            $commd->execute(array($id));
+            return $commd->fetch(PDO::FETCH_OBJ);
+        }catch(Throwable $t){
+            die($t->getMessage());
+        }
+    }
+
+    // Actualizar
+    public function actualizarMarca($data){
+        try{
+            // Comando SQL
+            $sql = "UPDATE marcas SET id = ?, nombre_marca = ?";
+
+            // COMENZAMOS LA CONEXION CON PDO
+            $pre = $this->DB->prepare($sql);
+            $resul = $pre->execute(array($data->id, $data->nombre_marca));
+            if($resul > 0){ 
+                return true;
+            }else{ 
+                return false;
+            }
+        }catch(Throwable $t){
+            die($t->getMessage());
+        }
+    }
+    // Borrar
+    public function BorrarMarca($data){
+        try{
+            // Comando SQL
+            $sql = "DELETE FROM productos  WHERE id = ?";
+            // COMENZAMOS LA CONEXION CON PDO
+            $pre = $this->DB->prepare($sql);
+            $resul = $pre->execute(array($data->id));
+            if($resul > 0){ 
+                return true;
+            }else{ 
+                return false;
+            }
+        }catch(Throwable $t){
+            die($t->getMessage());
+        }
+    }
+    // Para los mensajes
+    public function SesionesMessage($texto, $tipo){
+        $_SESSION['texto'] = $texto;
+        $_SESSION['tipo'] = $tipo;
+        header("Location: ?view=Marcas");
+    }
+
+}
+?>
